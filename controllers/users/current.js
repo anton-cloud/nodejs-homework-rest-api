7a156065e85292deb4083b87b0a1d7f1f559b8f0
Anton <a.sharudylo@gmail.com>
2021-11-18 09:11:53 +0200

    finish_04

---
+++ controllers/users/current.js
@@ -0,0 +1,11 @@
+const { User } = require("../../models");
+const { sendSuccessReq } = require("../../helpers");
+
+const current = async (req, res) => {
+  const id = req.user._id;
+  console.log(id);
+  const contact = await User.findById(id);
+  sendSuccessReq(res, { email: contact.email, subscription: contact.subscription });
+};
+
+module.exports = current;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,16 @@
 const express = require("express");
 
-const { controllerWrapper, validation } = require("../../middlewares");
+const { controllerWrapper, validation, authenticate } = require("../../middlewares");
 const { joinUserSchema } = require("../../models/user");
-const { auth: ctrl } = require("../../controllers");
+const { users: ctrl } = require("../../controllers");
 
 const router = express.Router();
 
 router.post("/signup", validation(joinUserSchema), controllerWrapper(ctrl.signup));
 
 router.post("/login", validation(joinUserSchema), controllerWrapper(ctrl.login));
+
+router.get("/current", authenticate, validation(joinUserSchema), controllerWrapper(ctrl.current));
 
 // router.get("/logout", controllerWrapper(ctrl.logout));
 
