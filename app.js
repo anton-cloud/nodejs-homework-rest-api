bef92ed8298eb25aca87bce9b5cc1980e40ee8e2
Anton <a.sharudylo@gmail.com>
2021-11-18 15:23:20 +0200

    Saving and overwriting a file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
+const multer = require("multer");
 
 const contactsRouter = require("./routes/api/contacts");
 const usersRouter = require("./routes/api/users");
@@ -16,7 +16,6 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", usersRouter);
 
@@ -28,5 +27,7 @@ app.use((error, req, res, next) => {
   const { status = 500, message = "Server error" } = error;
   res.status(status).json({ message });
 });
+
+// ==========================
 
 module.exports = app;
--- routes/api/users.js
+++ routes/api/users.js
@@ -14,4 +14,39 @@ router.post("/logout", authenticate, controllerWrapper(ctrl.logout));
 
 router.get("/current", authenticate, validation(joinUserSchema), controllerWrapper(ctrl.current));
 
+// ====================================
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs/promises");
+
+const tempDirectory = path.join(__dirname, "../../temp");
+const avatarsDirectory = path.join(__dirname, "../../public/avatars");
+
+const multerSetting = multer.diskStorage({
+  //куди зберегти
+  destination: (req, file, cb) => {
+    cb(null, tempDirectory);
+  },
+  //під яким ім'ям
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  //обмеження по розміру
+  limits: {
+    fileSize: 2058
+  }
+});
+
+//відділяє файл і текст
+const uploadMiddleware = multer({
+  storage: multerSetting
+});
+
+router.post("/avatars", uploadMiddleware.single("photo"), async (req, res) => {
+  const { path: tempStorage, originalname } = req.file; //старий шлях файлу
+  const resultStorage = path.join(avatarsDirectory, originalname); //новий шлях файлу
+  await fs.rename(tempStorage, resultStorage);
+});
+// ====================================
+
 module.exports = router;
